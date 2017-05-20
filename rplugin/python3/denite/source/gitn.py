7e00ecf49dd2b86a5c3b061052cf700c2a91b13b
KMNK <kmnknmk+com-github@gmail.com>
2017-05-20 18:01:21 +0900

    Modify gitn source as base source

--- rplugin/python3/denite/source/gitn.py
+++ rplugin/python3/denite/source/gitn.py
@@ -9,8 +9,7 @@ import sys
 print(os.path.dirname(__file__))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
-from gitn.source.factory import Factory
-from .base import Base
+from denite.source.base import Base
 
 class Source(Base):
 
@@ -18,26 +17,8 @@ class Source(Base):
         super().__init__(vim)
 
         self.name = 'gitn'
-
-    def on_init(self, context):
-        if not hasattr(self, '__source'):
-            self.__source = Factory.create(self, context)
-
-        if hasattr(self.__source, 'on_init'):
-            self.__source.on_init(context)
-
-        self.kind = self.__source.kind
-
-    def on_close(self, context):
-        if hasattr(self.__source, 'on_close'):
-            self.__source.on_close(context)
-
-    def highlight(self):
-        self.__source.highlight()
-
-    def define_syntax(self):
-        self.__source.define_syntax()
+        self.kind = 'gitn'
 
     def gather_candidates(self, context):
-        if hasattr(self.__source, 'gather_candidates'):
-            return self.__source.gather_candidates(context)
+        # TODO
+        return []
